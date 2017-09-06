b181f746c1ddc62210542497a816dfcb919ee768
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-09-06 23:58:51 +0530

    Added multiple component files

--- redux/tutorials/ReduxSimpleStarter/src/index.js
+++ redux/tutorials/ReduxSimpleStarter/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'; //React is based on two libraries <- making components
 import ReactDOM from 'react-dom'; ///Rendering components to the browser.
+
+import SearchBar from './components/search_bar.js';
+
 // Create a new component , produce some HTML.
+const api_key = 'AIzaSyCNHxbJ0sEJyNO-wtbSaWTi-8HpqnAQO2o';
 
 const App = function() {
-    return <div > Hi! </div>
+    return (
+    <div > 
+         <SearchBar/>
+    </div>
+    );
 }
 
 //Put the component on the DOM.
-ReactDOM.render(<App/>,document.querySelector('.container'));
+ReactDOM.render(<App/> , document.querySelector('.container'));
