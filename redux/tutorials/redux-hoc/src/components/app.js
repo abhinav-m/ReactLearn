340005b4dbb6a638eb3b8ca3353167720f8642d0
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-02-23 22:06:25 +0530

    Added various stuff from tutorial

--- redux/tutorials/redux-hoc/src/components/app.js
+++ redux/tutorials/redux-hoc/src/components/app.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Header from './header';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header/>
+        {this.props.children}
+      </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { posts: state.posts};
+}
+
+//Example of a higher order component.
+//Wrapping a react component inside a helper component.
+//The connect component is a higher order component which communicates with the provider and manages the redux store(app level state).
+export default connect(mapStateToProps)(App);
---
+++ redux/tutorials/redux-hoc/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div>
+    Recipe
+      <ul>
+      <li>abc</li>
+      <li>bcd</li>
+      <li>xyz</li>
+      </ul>
+    </div>
+  );
+};
