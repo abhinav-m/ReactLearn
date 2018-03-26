899fb2e0e69f0173d872005d699ce2ba70ab62a9
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-26 14:53:02 +0530

    Unstaging node_modules folder

--- egghead-react-tuts/react-ref-component/src/client/app/index.jsx
+++ egghead-react-tuts/react-ref-component/src/client/app/index.jsx
@@ -7,14 +7,14 @@ or an instance of a component
 in your application. */
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {a:'',b:''}
-    }
-    
-    update(e) {
-        this.setState({
-        /*  a:this.refs.a.value,
+  constructor() {
+    super();
+    this.state = { a: '', b: '' };
+  }
+
+  update(e) {
+    this.setState({
+      /*  a:this.refs.a.value,
             b:this.refs.b.value,
             When we use refs like this,
             it returns the node.
@@ -25,45 +25,41 @@ class App extends React.Component {
             This wont work when the component wraps the node we want to access in another DOM node.
             To overcome this,
             */
-            a:this.a.refs.input.value, //Now this will use the ref in our App component to return it's component ,
-                                       //which has its own ref and points to the components input node.
-            b:this.refs.b.value,
-        })
-    }
-    
-    render() {
-        return(
-        <div>
+      a: this.a.refs.input.value, //Now this will use the ref in our App component to return it's component ,
+      //which has its own ref and points to the components input node.
+      b: this.refs.b.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
         <Input
-        /* <input>  
+          /* <input>  
           type="text" 
           ref="a" we can use refs like this to assign a reference to the node directly
-        // or use a callback as :*/
-     //   ref= { node => this.a = node} // this.a will now refer to the node, and this.a.value will be its value, since the node is assigned to this.a in callback.
-       ref ={ component => this.a = component} //Same case here now, ref refers to the component instead of the node. therefore this.a = component in this case
-        update={this.update.bind(this)}
-        /> {this.state.a}
-        <hr/>
-             <input
-        type="text"
-        //  ref="b"
-        ref ="b"
-        onChange={this.update.bind(this)}
-        /> {this.state.b}
-        </div>
-        ) 
-    
-    }
+         // or use a callback as :*/
+          //   ref= { node => this.a = node} // this.a will now refer to the node, and this.a.value will be its value, since the node is assigned to this.a in callback.
+          ref={component => (this.a = component)} //Same case here now, ref refers to the component instead of the node. therefore this.a = component in this case
+          update={this.update.bind(this)}
+        />{' '}
+        {this.state.a}
+        <hr />
+        <input type="text" ref="b" onChange={this.update.bind(this)} />
+        {this.state.b}
+      </div>
+    );
+  }
 }
 
 class Input extends React.Component {
-    render() {
-        return (
-            <div>
-            <input type ="text" ref="input" onChange ={this.props.update}></input>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <input type="text" ref="input" onChange={this.props.update} />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>,document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
