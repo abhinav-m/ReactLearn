a5a186f27254fe32c90e7a956387c2d6ddd45b48
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-26 15:15:38 +0530

    Minor comments changes

--- egghead-react-tuts/react-ref-component/src/client/app/index.jsx
+++ egghead-react-tuts/react-ref-component/src/client/app/index.jsx
@@ -12,9 +12,7 @@ class App extends React.Component {
     this.state = { a: '', b: '' };
   }
 
-  update(e) {
-    this.setState({
-      /*  a:this.refs.a.value,
+  /*    a:this.refs.a.value,
             b:this.refs.b.value,
             When we use refs like this,
             it returns the node.
@@ -25,24 +23,32 @@ class App extends React.Component {
             This wont work when the component wraps the node we want to access in another DOM node.
             To overcome this,
             */
+
+  update(e) {
+    this.setState({
       a: this.a.refs.input.value, //Now this will use the ref in our App component to return it's component ,
       //which has its own ref and points to the components input node.
       b: this.refs.b.value
     });
   }
 
+  /* <input>  
+        type="text" 
+        ref="a" 
+        we can use refs like this to assign a reference to the node directly
+        or use a callback as 
+        ref= { node => this.a = node}:
+        this.a will now refer to the node, and this.a.value will be its value,
+         since the node is assigned to this.a in callback.
+  */
+
   render() {
     return (
       <div>
         <Input
-          /* <input>  
-          type="text" 
-          ref="a" we can use refs like this to assign a reference to the node directly
-         // or use a callback as :*/
-          //   ref= { node => this.a = node} // this.a will now refer to the node, and this.a.value will be its value, since the node is assigned to this.a in callback.
           ref={component => (this.a = component)} //Same case here now, ref refers to the component instead of the node. therefore this.a = component in this case
           update={this.update.bind(this)}
-        />{' '}
+        />
         {this.state.a}
         <hr />
         <input type="text" ref="b" onChange={this.update.bind(this)} />
