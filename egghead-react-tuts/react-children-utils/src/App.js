9db378b054df21b562af5ee6143827ee0dd59d4c
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-26 15:21:07 +0530

    Minor changes and comments

--- egghead-react-tuts/react-children-utils/src/App.js
+++ egghead-react-tuts/react-children-utils/src/App.js
@@ -5,23 +5,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-   <Parent>
-   <div className='childA'></div>
-   </Parent>
+      <Parent>
+        <div className="childA">test</div>
+      </Parent>
     );
   }
 }
 
-class Parent extends React.Component {
-  render() {
-    /*Example map call when we expect this.props.children to be an array.
+/*Example map call when we expect this.props.children to be an array.
     this.props.children.map( v=> v)
     This will work when their is more than one child in this.props.children.
     If there is one child , we cant use map. To solve this, we use
     React.children which has many utility functions to use against react components.
     console.log(this.props.children) */
 
-    /* The map function takes two arguments first what children do you want to perform 
+/* The map function takes two arguments first what children do you want to perform 
      the mapping against and second the mapping function.
      let items = React.Children.map(this.props,children, child => child )
      Another way to do this is:
@@ -30,9 +28,12 @@ class Parent extends React.Component {
      React.Children.forEach( this.props.children,child => console.log(child.props.className))
      Only function returns child if theres only ONE child in the component,
      otherwise throws error. */
-     let items = React.Children.only(this.props.children)
-    console.log(items)
-    return null;
+
+class Parent extends React.Component {
+  render() {
+    let items = React.Children.only(this.props.children);
+    console.log(items);
+    return items;
   }
 }
 export default App;
